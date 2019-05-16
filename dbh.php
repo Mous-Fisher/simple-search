1c295f11991064c529600a61e1accbb09ff29a6a
MousFisher <44139455+Mous-Fisher@users.noreply.github.com>
2019-05-16 04:08:18 +0200

    Add files via upload

---
+++ dbh.php
@@ -0,0 +1,7 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbName = "searchdbh";
+
+$conn = mysqli_connect($server,$username,$password,$dbName);
---
+++ header.php
@@ -0,0 +1,15 @@
+<?php include "dbh.php"
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic search field</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php
+	
+
+?>
